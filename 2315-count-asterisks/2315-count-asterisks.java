7fea3b30ad661e19a4232e7e839f629763f5fd31
Keerti Karn <keertikarn2005@gmail.com>
2026-02-28 17:38:06 +0530

    Time: 3 ms (34.09%), Space: 42.9 MB (86.23%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int bar=0;
+        int count=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='|') bar++;
+            else if(bar%2==0 && s.charAt(i)=='*') count++;
+        }
+        return count;
+    }
+}
