8bff79970e3f3aeb2bee29dd284d46b6f1031df5
Keerti Karn <keertikarn2005@gmail.com>
2026-04-24 11:16:35 +0530

    Time: 4 ms (32.06%), Space: 101.2 MB (80.2%) - LeetHub

---
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int waysToSplitArray(int[] nums) {
+        long[] prefix= new long[nums.length];
+        prefix[0]=nums[0];
+        for(int i=1;i<nums.length;i++) prefix[i]=prefix[i-1]+nums[i];
+        int count=0;
+        for(int i=0;i<nums.length-1;i++){
+            long leftsum=prefix[i];
+            long rightsum= prefix[prefix.length-1]-prefix[i];
+            if(leftsum>=rightsum) count++;
+        }
+        return count;
+    }
+}
