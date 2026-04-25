4695f67fcf4c7ad9ae23d54a9600d063fde2dd6f
Keerti Karn <keertikarn2005@gmail.com>
2026-04-25 11:15:41 +0530

    Time: 1 ms (30.52%), Space: 42.6 MB (69.89%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> st1= new Stack<>();
+        Stack<Integer> st2= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch= s.charAt(i);
+            if(ch=='(') st1.push(i);
+            else if(ch==')'){
+                if(!st1.isEmpty()) st1.pop();
+                else if(!st2.isEmpty()) st2.pop();
+                else return false;
+            }
+            else st2.push(i);
+        }
+        while(!st1.isEmpty() && !st2.isEmpty()){
+          if(st1.peek()<st2.peek()){  //"(" comes before *
+             st1.pop();
+             st2.pop();
+          }
+          else return false;
+        }
+        return st1.isEmpty();
+    }
+}
