e9cac58e4b579f2604dadcf1f4f58f98db11530c
Keerti Karn <keertikarn2005@gmail.com>
2026-04-29 09:41:04 +0530

    Time: 0 ms (100%), Space: 43 MB (39.46%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String breakPalindrome(String palindrome) {
+        if(palindrome.length()==1) return "";
+        StringBuilder ans= new StringBuilder();
+        ans.append(palindrome);
+        for(int i=0;i<palindrome.length()/2;i++){
+            if(palindrome.charAt(i)!='a') {ans.setCharAt(i,'a');
+            return ans.toString();}
+        }
+         ans.setCharAt(palindrome.length()-1,'b');
+        return ans.toString();
+    }
+}
