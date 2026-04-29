13f9d0d323c56520fc7b7988d19692c7d8e63659
Keerti Karn <keertikarn2005@gmail.com>
2026-04-29 10:46:51 +0530

    Time: 15 ms (95.06%), Space: 83.9 MB (16.86%) - LeetHub

---
+++ 2611-mice-and-cheese/2611-mice-and-cheese.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int miceAndCheese(int[] reward1, int[] reward2, int k) {
+        int n= reward1.length;
+        int total= 0;
+        int[] diff= new int[n];
+        for(int i=0;i<n;i++){
+            total+= reward2[i];
+            diff[i]= reward1[i]-reward2[i];
+        }
+        Arrays.sort(diff);
+        for(int i=n-1;i>=n-k;i--){
+            total+= diff[i];
+        }
+        return total;
+    }
+}
