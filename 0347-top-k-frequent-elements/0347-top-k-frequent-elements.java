1928186adfd2783407089aa9e55393d2cecdfcb1
Keerti Karn <keertikarn2005@gmail.com>
2026-05-13 14:44:44 +0530

    Time: 17 ms (19.83%), Space: 47.6 MB (47.61%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,53 +1,24 @@
-// class Pair implements Comparable<Pair>{
-//   int num;
-//   int freq;
-//   Pair(int num,int freq){
-//     this.num=num;
-//     this.freq=freq;
-//   }
-//   public int compareTo(Pair p){
-//    return this.freq-p.freq;
-//   }
-// }
-
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] ans = new int[k];
-        HashMap<Integer,Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map= new HashMap<>();
         for(int ele: nums){
-            if(map.containsKey(ele)){
-                map.put(ele,map.get(ele)+1);
-            }
-            else{
-                map.put(ele,1);
-            }
+            if(map.containsKey(ele)) map.put(ele,map.get(ele)+1);
+            else map.put(ele,1);
         }
-        PriorityQueue<Integer> pq = new PriorityQueue<>((b,a)->{
-            int d1= map.get(a);
-            int d2= map.get(b);
-            return d1-d2;
-        });
-        for (Map.Entry<Integer,Integer> entry : map.entrySet()){
-            pq.add(entry.getKey());
+        PriorityQueue<Integer> pq= new PriorityQueue<>((a,b)->{
+            int x= map.get(a);
+            int y=map.get(b);
+            return y-x;
         }
+        );
+        for(int ele: map.keySet()) pq.add(ele);
+        int[] ans= new int[k];
         int i=0;
-        while(!pq.isEmpty() && k>0){
+        while(k>0){
             ans[i]=pq.remove();
-            i++;
             k--;
+            i++;
         }
-        //   for (Map.Entry<Integer,Integer> entry : map.entrySet()){
-        //     int num = entry.getKey();
-        //     int diff= entry.getValue();
-        //     pq.add(new Pair(num,diff));
-        //     if(pq.size()>k){
-        //         pq.remove();
-        //     }
-        //   } 
-        //   for(int i=0;i<k;i++){
-        //    Pair p = pq.remove();
-        //    ans[i] = p.num;
-        //   }
-         return ans;
+        return ans;
     }
 }
