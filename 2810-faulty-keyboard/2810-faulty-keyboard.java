7fb61043ff782ac4ac40d2168f53a954476daf96
Keerti Karn <keertikarn2005@gmail.com>
2026-05-28 21:16:30 +0530

    Time: 3 ms (97.07%), Space: 46.3 MB (82.58%) - LeetHub

--- 2810-faulty-keyboard/2810-faulty-keyboard.java
+++ 2810-faulty-keyboard/2810-faulty-keyboard.java
@@ -2,20 +2,10 @@ class Solution {
     public String finalString(String s) {
         StringBuilder sb= new StringBuilder();
         for(int x=0;x<s.length();x++){
-            if(s.charAt(x)=='i'){
-                int i=0;
-                int j=sb.length()-1;
-                while(i<j){
-                    char temp= sb.charAt(i);
-                    sb.setCharAt(i,sb.charAt(j));
-                    sb.setCharAt(j,temp);
-                    i++;
-                    j--;
-
-                }
+            if(s.charAt(x)=='i') sb.reverse();
+             else sb.append(s.charAt(x));
             }
-            else sb.append(s.charAt(x));
+                return sb.toString();
         }
-        return sb.toString();
+   
     }
-}
