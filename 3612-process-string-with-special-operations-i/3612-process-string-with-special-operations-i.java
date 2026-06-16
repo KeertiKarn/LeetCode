fb74a002387917dc10e0ad0a85d2b93e1601131c
Keerti Karn <keertikarn2005@gmail.com>
2026-06-16 09:47:49 +0530

    Time: 4 ms (76.47%), Space: 55.3 MB (48.53%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb= new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            char ch= s.charAt(i);
+            if(Character.isLowerCase(ch)) sb.append(ch);
+            else if(ch=='*' && sb.length()>0) sb.deleteCharAt(sb.length()-1);
+            else if(ch=='#') sb.append(sb.toString());
+            else sb.reverse();
+        }
+        return sb.toString();
+    }
+}
