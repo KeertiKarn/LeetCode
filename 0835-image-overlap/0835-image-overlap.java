e513d7089bbe5e900bb894cf7ca710738abeedbd
Keerti Karn <keertikarn2005@gmail.com>
2026-09-13 10:37:02 +0530

    Time: 184 ms (22.73%), Space: 48 MB (8.71%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.java
@@ -0,0 +1,44 @@
+class Solution {
+
+    public int largestOverlap(int[][] img1, int[][] img2) {
+
+        int n = img1.length;
+
+        List<int[]> A = new ArrayList<>();
+        List<int[]> B = new ArrayList<>();
+
+        // collect coordinates of 1s
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (img1[i][j] == 1)
+                    A.add(new int[]{i, j});
+
+                if (img2[i][j] == 1)
+                    B.add(new int[]{i, j});
+            }
+        }
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        int ans = 0;
+
+        for (int[] a : A) {
+            for (int[] b : B) {
+
+                int dx = b[0] - a[0];
+                int dy = b[1] - a[1];
+
+                String key = dx + "," + dy;
+
+                int count = map.getOrDefault(key, 0) + 1;
+
+                map.put(key, count);
+
+                ans = Math.max(ans, count);
+            }
+        }
+
+        return ans;
+    }
+}
